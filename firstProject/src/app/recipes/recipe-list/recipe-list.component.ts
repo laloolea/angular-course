188c66c99868c8e38d97f0b57f964f4292387694
Eduardo Olea <laloolea18@gmail.com>
2021-12-22 11:38:07 -0700

    Finished ch3 and 4 initialized my first Project

---
+++ firstProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit { 
+  recipes:Recipe[]=[
+    new Recipe("Sushi","A very tasty Japanese recipe","https://cdn.dribbble.com/users/2532154/screenshots/5335283/media/37266057842300c208df874094c1f45f.png?compress=1&resize=1600x1200"),
+    new Recipe("Sushi","A very tasty Japanese recipe","https://cdn.dribbble.com/users/2532154/screenshots/5335283/media/37266057842300c208df874094c1f45f.png?compress=1&resize=1600x1200")     
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
