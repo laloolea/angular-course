eee8bd4487e3ea30a30134216caa7061936e27fd
Eduardo Olea <laloolea18@gmail.com>
2021-12-23 12:29:27 -0700

    finished ch6

--- firstProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ firstProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit { 
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[]=[
     new Recipe("Sushi","A very tasty Japanese recipe","https://cdn.dribbble.com/users/2532154/screenshots/5335283/media/37266057842300c208df874094c1f45f.png?compress=1&resize=1600x1200"),
-    new Recipe("Sushi","A very tasty Japanese recipe","https://cdn.dribbble.com/users/2532154/screenshots/5335283/media/37266057842300c208df874094c1f45f.png?compress=1&resize=1600x1200")     
+    new Recipe("Onigiri","Another very tasty Japanese recipe","https://www.clipartmax.com/png/middle/22-221628_wasabi-onigiri-cartoon.png")     
   ];
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
