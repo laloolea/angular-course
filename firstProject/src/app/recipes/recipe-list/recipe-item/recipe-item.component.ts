f7542427272234de2c650b7d80cfb8acaee22f5b
Eduardo Olea <laloolea18@gmail.com>
2021-12-28 13:43:35 -0700

    finished ch12 added routing to my first project

--- firstProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ firstProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,16 +9,12 @@ import { RecipeService } from '../../recipe.service';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe: Recipe;
-
-
-  constructor(private recipeService:RecipeService) { }
+  @Input() recipe : Recipe;
+  @Input() index : number;
 
   ngOnInit() {
+
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe); 
-  }
 
 }
