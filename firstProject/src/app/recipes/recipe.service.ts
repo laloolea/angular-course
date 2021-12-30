ab4d15f44c589ea2dbbe2c88521e2d0b4fe841ca
Eduardo Olea <laloolea18@gmail.com>
2021-12-30 10:44:07 -0700

    Finished ch16 added Reactive Forms to FirstProject

--- firstProject/src/app/recipes/recipe.service.ts
+++ firstProject/src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService{
+    recipesChanged=new Subject<Recipe[]>();
     recipeSelected= new EventEmitter<Recipe>();
     private recipes:Recipe[]=[
         new Recipe("Sushi",
@@ -37,4 +39,16 @@ export class RecipeService{
     getRecipe(index:number){
         return this.recipes[index];
     }    
+    addRecipe(recipe:Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+    updateRecipe(index:number,newRecipe:Recipe){
+        this.recipes[index]=newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+    deleteRecipe(index:number){
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
 }
