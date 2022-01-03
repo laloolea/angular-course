cce1069ca5e2de79ccd7e0d6e65307a23d0b8413
Eduardo Olea <laloolea18@gmail.com>
2022-01-03 13:29:22 -0700

    Finished ch 18 about Http Requests using firebase as the backend

---
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -0,0 +1,17 @@
+import {  HttpHandler,
+     HttpInterceptor,
+      HttpRequest }
+       from "@angular/common/http";
+
+
+
+export class AuthInterceptorService implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler){
+        console.log("Request is on this way");
+        const modifiedRequest = req.clone({
+            headers:req.headers.append('Auth','xyz')
+        });
+        return next.handle(modifiedRequest);
+    }
+
+}
